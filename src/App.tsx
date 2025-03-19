26327544fdfdbf5396c1b5b8318cd553ee0a7584
PABourdais <pierre.alex.bourdais@gmail.com>
2025-03-19 11:32:19 +0100

    feat(App.tsx): edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, CardContent, Typography } from "@mui/material";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card sx={{ minWidth: 275, padding: 2 }}>
+      <CardContent>
+        <Typography variant="h5">Hello, this is my ETH extension!</Typography>
+      </CardContent>
+    </Card>
   );
-}
+};
 
 export default App;
